be7989708e4c46df5453fd62c4e55ba242fab650
Onur Tosyalıoğlu <onurtosyalioglu@gmail.com>
2023-11-27 15:52:28 +0300

    [DEV] modules scaffold

--- module.sh
+++ module.sh
@@ -3,7 +3,10 @@ mkdir -p ./modules/$1
 mkdir -p ./modules/$1/presentation
 mkdir -p ./modules/$1/presentation/context/
 mkdir -p ./modules/$1/presentation/layout/
+mkdir -p ./modules/$1/presentation/layout/index/
 mkdir -p ./modules/$1/presentation/component/
+mkdir -p ./modules/$1/presentation/component/index/
+mkdir -p ./modules/$1/presentation/component/common/
 
 mkdir -p ./modules/$1/application
 mkdir -p ./modules/$1/application/service
